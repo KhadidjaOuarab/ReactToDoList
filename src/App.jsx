cc6116f50f6800683ddb5c73d3a95de7c3fa57a8
ouarab.khadidja <ouarab.khadidja@gmail.com>
2022-01-09 22:32:07 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import ToDoList from './Components/ToDoList'
 import axios from 'axios'
+//import { ThemeContext, themes } from './Components/themeContext';
+//import ToggleDark from './components/toggleDark';
 
 function App() {
-
+  const [darkMode, setDarkMode] = useState(true);
   const [inputValue, setInputValue] = useState("");
   const [state, setstate] = useState([])
-  const [countAllTasks, setCountAllTasks] = useState(0);
   const [countCompleted, setCountCompleted] = useState(0);
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/todos')
-      .then(res => { setstate(res.data.slice(0, 10)) })
+      .then(res => {
+        setstate(res.data.slice(0, 10))
+        for (let index = 0; index < res.data.slice(0, 10).length; index++) {
+          if (res.data[index].completed) {
+            setCountCompleted((countCompleted) => countCompleted + 1)
+          }
+
+        }
+
+      })
 
   }
     , [])
 
-    useEffect(() => {
-      axios.get('https://jsonplaceholder.typicode.com/todos')
-      
-     .then(res => {if (res.completed) setCountCompleted(countCompleted++)  })
-    
+  const checkedList = (e) => {
+    if (e.currentTarget.checked) {
+      setCountCompleted((countCompleted) => countCompleted + 1)
     }
-      , [])
-   
+    else {
+      setCountCompleted((countCompleted) => countCompleted - 1)
+    }
+  }
+
   const getValue = (e) => {
     setInputValue(e.target.value)
   }
@@ -31,15 +42,17 @@ function App() {
   const printValue = (e) => {
     e.preventDefault()
     console.log(inputValue);
-    let id = state.length +1
+    let id = state.length + 1
     state.push({ id: id, title: inputValue })
     setInputValue("")
   }
   return (
     <div >
-      <ToDoList  printValue={printValue} getValue={getValue} inputVal={inputValue} choix={state}
-      countAllTasks={state.length} countCompleted={countCompleted} />
+      <ToDoList printValue={printValue} getValue={getValue} inputVal={inputValue} choix={state}
+        countComplete={checkedList} countAllTasks={state.length} countCompleted={countCompleted} />
+
      
+
     </div>
   )
 
--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
-function ToDoList({ choix, getValue, printValue, inputVal, countAllTasks,countCompleted }) {
+function ToDoList({ countComplete, choix, getValue, printValue, inputVal, countAllTasks, countCompleted }) {
+    const [isChecked, setIsChecked] = useState(false);
     return (
-        <div >
+        <div  className='bg-white text-center '>
             <form onSubmit={printValue} >
-                <input onChange={getValue} type="text" value={inputVal}></input>
-                <button type='submit'  >Add</button>
+                <h1 className='font-bold '>T O D O</h1>
+                <input className='border-2 w-80 h-9' onChange={getValue} type="text" value={inputVal}></input>
+                <button type='submit' className='w-28 border-2 ml-5' >Add</button>
             </form>
-
             <div>
-                <ul>
+                <ul className='flex flex-col content-center ml-96 text-center mt-11'>
                     {choix.map((ele) => (
-                    <li key={ele.id} className='flex flex-row space-x-7 text-lg'>
-                        <input type="checkbox" defaultChecked={ele.completed} ></input>
-                        <p> {ele.id} {ele.title} </p>
-                    </li>))}
+                        <li key={ele.id} className='flex flex-row space-x-7 text-lg'>
+                            <input onClick={countComplete}
+                                type="checkbox" defaultChecked={ele.completed} ></input>
+                            <p> {ele.id} {ele.title} </p>
+                        </li>))}
                 </ul>
             </div>
-           
-      
-            <p>All Tasks</p><input value={countAllTasks} />
-            <p>Completed Tasks</p><input value={countCompleted} />
-
+            <div className='ml-96 flex flex-row mt-7 gap-4'><p>All Tasks</p><input value={countAllTasks} /></div>
             
-    
+            <div className='ml-96 flex flex-row gap-4'><p>Completed Tasks</p><input value={countCompleted} /></div>
+
+
+
         </div>
     )
 }
